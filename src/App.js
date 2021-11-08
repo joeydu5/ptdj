8b3e44bae1b9e34d030cef1d08a2bbd2daff8090
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2021-11-09 08:45:16 +1100

    styling home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div className="app">
+      <Home />
     </div>
   );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,43 @@
+
+import React from "react";
+import "./footer.styles.scss";
+import SignFooter from './../../images/sign_footer.svg';
+import  insta from './../../images/instagram.svg';
+import  twitter from './../../images/twitter.svg';
+import  facebook from './../../images/facebook.svg';
+
+
+const Footer = ()=> {
+    return (
+    <section className="footer">
+        <div className="footer__container">
+            <img src={SignFooter} alt="sign" />
+
+            <div className="footer__social">
+                <h3>SOCIAL</h3>
+                <img width="48px" height="48" src={facebook} alt="Facebook" />
+                <img width="48" height="48"  src={insta} alt="Instagram" />
+                <img width="48" height="48"  src={twitter} alt="Twitter" />
+            </div>
+
+            <div className="footer__our">
+                <h3>OUR LINKS</h3>
+                <p>Home</p>
+                <p>Find A Coach</p>
+                <p>Become A Coach</p>
+                <p>About Us</p>
+                <p>How It Works</p>
+                <p>Contact Us</p>
+            </div>
+
+            <div className="footer__about">
+                <h3>ABOUT</h3>
+                <p>Legals</p>
+                <p>Privacy</p>
+            </div>
+        </div>
+        <p className="footer__copyright">© PTDJ2021</p>
+    </section>)
+}
+
+export default Footer;
